d35a2870c63321e4b2b70a16bd30b3ff20071370
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-08-02 23:37:29 -0600

    Created reaction.js

---
+++ models/reaction.js
@@ -0,0 +1,38 @@
+const { Schema, Types } = require('mongoose');
+
+// Importing momentFormatDate.js for time sheet of when reaction is created
+const allotedTime = require('../utils/momentFormatDate');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String, 
+            required: true, 
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String, 
+            required: true
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now, 
+            get: (time) => allotedTime(time)
+        }
+    },
+    // Data for each of these routes is displayed in a formatted JSON
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+          },
+          id: false,
+    }
+);
+
+module.exports = reactionSchema
